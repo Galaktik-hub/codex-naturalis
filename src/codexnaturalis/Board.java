92018299143a7c7d901728df83a4177ad0bb2ce5
Kirushikesan <148065907+Kirushield@users.noreply.github.com>
2024-04-03 12:31:18 +0200

    Tips du prof concernant board

--- src/codexnaturalis/Board.java
+++ src/codexnaturalis/Board.java
@@ -2,6 +2,7 @@ package codexnaturalis;
 
 public class Board {
 	public Board() {
-		
+//		Dans une matrice les coordonnées doivent être des cartes première carte (0,0)
+//		Avec BoardView et méthode scale on peut redimensionner
 	}
 }
