3b9577f06b86de60dfcdfc7518d802ff58c56d2e
Alexis <56489898+Galaktik-hub@users.noreply.github.com>
2024-04-05 09:36:29 +0200

    Removed useless sizes functions and size variable

--- src/codexnaturalis/card/Deck.java
+++ src/codexnaturalis/card/Deck.java
@@ -5,26 +5,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
+	/*
+	 * Deck of card is represented by a list of Card.
+	 */
 	private final List<Card> deck;
-	private int size;
 	
 	public Deck() {
 		this.deck = new ArrayList<>();
-		this.size = 0;
 		fill();	// À l'initialisation du deck, on le rempli
 		shuffle();	// Puis on le mélange
 	}
-	
-	public void incrementDeckSize() {
-		this.size++;
-	}
-	
-	public void decrementDeckSize() {
-		this.size--;
-	}
 
 	public int getSize() {
-		return size;
+		return deck.size();
 	}
 	
 	public void shuffle() {
@@ -33,29 +26,28 @@ public class Deck {
 	
 	public Card drawCard() {
 		Card card = deck.removeLast();
-		decrementDeckSize();
 		return card;
 	}
 	
 	public boolean isEmpty() {
-		return this.size == 0;
+		return getSize() == 0;
 	}
 
 	private void fill() {
-		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, RessourceType.ANIMAL, RessourceType.NONE, 0)); incrementDeckSize();
-		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.ANIMAL, RessourceType.NONE, RessourceType.NONE, RessourceType.ANIMAL, 0)); incrementDeckSize();
-		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.ANIMAL, RessourceType.NONE, RessourceType.NONE, RessourceType.NONE, 0)); incrementDeckSize();
-		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, 0)); incrementDeckSize();
-		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, RessourceType.NONE, 0)); incrementDeckSize();
-		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, RessourceType.NONE, RessourceType.NONE, 1)); incrementDeckSize();
-		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, RessourceType.ANIMAL, RessourceType.NONE, 1)); incrementDeckSize();
+		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, RessourceType.ANIMAL, RessourceType.NONE, 0));
+		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.ANIMAL, RessourceType.NONE, RessourceType.NONE, RessourceType.ANIMAL, 0));
+		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.ANIMAL, RessourceType.NONE, RessourceType.NONE, RessourceType.NONE, 0));
+		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, 0));
+		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, RessourceType.NONE, 0)); 
+		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, RessourceType.NONE, RessourceType.NONE, 1));
+		this.deck.add(new RessourceCard(RessourceType.ANIMAL, RessourceType.NONE, RessourceType.ANIMAL, RessourceType.ANIMAL, RessourceType.NONE, 1));
 	}
 	
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Contenu du deck de taille ")
-			.append(this.size)
+			.append(getSize())
 			.append("\n---\n");
 		for (Card card : deck) {
 			sb.append(card.toString())
