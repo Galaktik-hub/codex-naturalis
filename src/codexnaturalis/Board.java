47cd2b855834f786f22b503e76e063cfc8c63beb
Alexis <56489898+Galaktik-hub@users.noreply.github.com>
2024-04-05 09:24:49 +0200

    Add files via upload

--- src/codexnaturalis/Board.java
+++ src/codexnaturalis/Board.java
@@ -1,6 +1,11 @@
 package codexnaturalis;
 
 public class Board {
+	/*
+	 * Creation of a 2d matrix to represent game view
+	 */
+	public int[][] board;
+	
 	public Board() {
 //		Dans une matrice les coordonnées doivent être des cartes première carte (0,0)
 //		Avec BoardView et méthode scale on peut redimensionner
--- src/codexnaturalis/CodexNaturalis.java
+++ src/codexnaturalis/CodexNaturalis.java
@@ -1,6 +1,7 @@
 package codexnaturalis;
 
 import java.awt.Color;
+import codexnaturalis.card.Coordinates;
 import codexnaturalis.card.Deck;
 import codexnaturalis.player.Player;
 import fr.umlv.zen5.Application;
@@ -14,27 +15,19 @@ public class CodexNaturalis {
 		
 		System.out.println(p1);
 		System.out.println(deck);
-		Application.run(Color.ORANGE, context -> {
-			
-			// Attribution des positions des cartes
-			int x = 40;
-			int y = 40;
-			int positionCard = 0; // ième position de la carte dans le deck
-			int widthCard = 200; // Largeur de la carte afin de donner une marge
-			
+		Application.run(Color.ORANGE, context -> {			
 			// Boucle du jeu
 			while (true) {
 				var event = context.pollOrWaitEvent(10); // Récuperer un event
 				if (event == null) {
-					continue; // Si il n'y a pas de event on coninue
+					continue;
 				}
-				var action = event.getAction(); // Si il y a un event, récupérer l'action
+				var action = event.getAction();
 				if (action == Action.POINTER_DOWN) { // Si clic de la souris
-					if (deck.getSize() - 1 >= positionCard) { // Si la position de la carte est dans le deck
-						deck.get(positionCard).draw(context, x, y); // On dessine avec la méthode draw
-						x += widthCard+50; // Un espacement entre les cartes
-						positionCard++; // La carte suivante
-					} else { // Si la position de la carte pas dans le deck
+					if (!(deck.isEmpty())) { // Tant que le deck n'est pas vide
+						Coordinates coordinates = new Coordinates(event.getLocation().x, event.getLocation().y);
+						deck.drawCard().draw(context, coordinates);
+					} else { // Sinon, on quitte le jeu
 						System.out.println("Deck épuisé");
 						context.exit(0); // On ferme la fenêtre
 						return;
